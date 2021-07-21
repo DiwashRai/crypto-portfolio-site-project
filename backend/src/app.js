1086fe5d5586b3bba85a9797b74fce1c8690977f
Diwash Rai <diwash.rai1993@gmail.com>
2021-07-21 23:51:00 +0100

    setting up testing for backend part of project

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ const app = express();
 const router = new express.Router();
 
 router.get('*', (req, res) => {
-    res.send({ message: 'Hello world!'});
+    res.status(404).send({ error: 'Invalid route'});
 });
 
 app.use(express.json());
