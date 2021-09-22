87b96e4d3b1720e9145db2a07b76a07c77636a73
Diwash Rai <diwash.rai1993@gmail.com>
2021-09-22 23:53:23 +0100

    Changed auth middleware

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -10,7 +10,6 @@ const auth = async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findOne({
       _id: decoded._id,
-      'tokens.token': token,
     });
 
     if (!user) {
