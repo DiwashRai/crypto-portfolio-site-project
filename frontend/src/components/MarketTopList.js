46069e20d1428ec12bb05edd36138a90b879620d
Diwash Rai <diwash.rai1993@gmail.com>
2021-10-20 23:51:44 +0100

    Experimenting with Sparklines

--- frontend/src/components/MarketTopList.js
+++ frontend/src/components/MarketTopList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectMarketData } from '../reducers/marketDataReducer';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 const MarketTopList = () => {
   const marketData = useSelector((state) => selectMarketData(state));
@@ -15,14 +16,17 @@ const MarketTopList = () => {
           marketData.map((row) => (
             <div className="market-data-row">
               <div>
-                <img src={row.image} height="20" />
+                <img src={row.image} height="30" />
               </div>
               <div>
                 <span> {row.symbol.toUpperCase()}</span>
               </div>
               <div>
-                <span> {row.current_price}</span>
+                <span> {'$' + row.current_price}</span>
               </div>
+              <Sparklines data={row.sparkline_in_7d.price}>
+                <SparklinesLine color="blue" />
+              </Sparklines>
             </div>
           ))}
       </div>
